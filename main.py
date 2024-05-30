1c337b4656b02408908804f2dc2a1538d1404f44
Dipti Singh <dipti1010.singh@gmail.com>
2024-05-30 18:18:02 +0530

    data ingestion component added

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from src.Classifier import logger
+from src.Classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Hello from main.py")
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
